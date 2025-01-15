bf1ccfda4b9c92c235a155695b008eb9dfe9c308
ranjit <ranjitkumarnayak420@gmail.com>
2025-01-15 08:38:29 +0530

    navbar

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,23 @@
+import Logo from "@/components/ui/Logo";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <div className="border-b">
+        <nav className="flex items-center justify-between max-w-7xl mx-auto py-2">
+          <Logo />
+          <div className="flex items-center">
+            <Button variant={'link'}>DashBoard</Button>
+            <UserButton/>
+          </div>
+        </nav>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default layout;
